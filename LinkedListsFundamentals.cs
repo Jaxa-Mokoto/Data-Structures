8fefb7ae1829d7432609fb7d8b5291d67249b384
Jaxa Mokoto <47665098+Jaxa-Mokoto@users.noreply.github.com>
2022-04-17 01:30:57 +0800

    Update and rename LinkedListsFundamentals to LinkedListsFundamentals.cs

--- LinkedListsFundamentals.cs
+++ LinkedListsFundamentals.cs
@@ -49,6 +49,38 @@ public class MainClass
             head = node; // add reference to the new node i.e. pointer
             numberOfElementsInLinkedList++;
         }
+        
+        // method to add to the end of a linked list
+        public void AppendLinkedList(int data) 
+        {
+              //1. allocate node
+            LinkedListNode node = new LinkedListNode(data);
+              
+              //2. assign data element
+              node.data = data;
+              
+              //3. assign null to the next of new node
+              node.next = null; 
+              
+              //4. Check the Linked List is empty or not,
+              //   if empty make the new node as head
+              if(head == null) {
+                head = node;
+              } 
+              else 
+              {
+                //5. Else, traverse to the last node
+                LinkedListNode temp = new LinkedListNode(data);
+                temp = head;
+                
+                while(temp.next != null)
+                  temp = temp.next;
+                  numberOfElementsInLinkedList++; 
+                
+                //6. Change the next of last node to new node
+                temp.next = node;
+              }
+        }
 
         // Simple method to print all items in the linked list
         public void PrintList()
@@ -58,7 +90,7 @@ public class MainClass
             // Must check if the current data is not null prior to accessing the data
             while (runner != null)
             {
-                Console.WriteLine(runner.data);
+                Console.WriteLine($"My Linked List contains: {runner.data}");
 
                 // goes to the next item to print out
                 runner = runner.next;
@@ -72,15 +104,17 @@ public class MainClass
         LinkedList myLinkedList = new LinkedList();
 
         // Add new elements to the linked list
-        myLinkedList.AddNodeToFront(5);
-        myLinkedList.AddNodeToFront(55);
         myLinkedList.AddNodeToFront(2);
         myLinkedList.AddNodeToFront(23);
         myLinkedList.AddNodeToFront(4);
         myLinkedList.AddNodeToFront(55);
         myLinkedList.AddNodeToFront(6);
         myLinkedList.AddNodeToFront(78);
-        myLinkedList.AddNodeToFront(6);
+        myLinkedList.AddNodeToFront(78);
+        myLinkedList.AddNodeToFront(55);
+        
+        myLinkedList.AppendLinkedList(5);
+        myLinkedList.AppendLinkedList(2222);
         myLinkedList.PrintList();
     }
 }
