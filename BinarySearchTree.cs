3b30a3011f5c173dadcea1ed6eb0cd59a4c82040
Jaxa Mokoto <47665098+Jaxa-Mokoto@users.noreply.github.com>
2022-04-25 01:12:08 +0800

    Update BinarySearchTree.cs

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -1,75 +1,195 @@
-// Online C# Editor for free
-// Write, Edit and Run your C# code using C# Online Compiler
-    
 using System;
-namespace binarySearchTree
-{
-    public class binarySearchTree
-    {
-        public class Node 
-        {
-            public Node leftNode;
-            public Node rightNode;
-            public int data;
-            
-            public void DisplayNode()
-            {
-                Console.Write(data + " ");
-            }
-                
-            public Node(int value)//constructor
-            {
-                this.leftNode = null;
-                this.rightNode = null;
-                this.data = value;
-            }
-        }
-        
-        public Node root;
-        
-        public binarySearchTree() //constructor
-        {
-            this.root = null;
-        }
-        
-        public void insert(int value) //
-        {
-            //create the new node
-            Node newNode = new Node(value);
-            newNode.data = value;
-            //check if tree is empty, if so, add new node as root node
-            if (this.root == null)
-            {
-                this.root = newNode;
-            }
-            
-            //else, if there are existing nodes
-            
-        }
-        
-        //public void lookup(int data) {}
-    
-        public static void Main(string[] args)
-        {
-            binarySearchTree tree = new binarySearchTree();
-            tree.insert(9);
-            //tree.insert(4)
-            //tree.insert(6)
-            //tree.insert(20)
-            //tree.insert(170)
-            //tree.insert(15)
-            //tree.insert(1)
-        }
-        
-        //public binarySearchTree traverse(node) 
-        //{
-            // tree = new binarySearchTree();
-            //tree = ( value, node.value );
-            // condition ? consequent : alternative
-            //tree.left = node.left == null ? null : traverse(node.left);
-            //tree.right = node.right == null ? null : traverse(node.right);
-            //return tree;
-        //}
-    }
 
+class Node
+{
+    public Node LeftNode { get; set; }
+    public Node RightNode { get; set; }
+    public int Data { get; set; }
 }
+
+class BinaryTree
+{
+    public Node Root { get; set; }
+ 
+    public bool Insert(int value)
+    {
+        Node newNode = new Node();
+        newNode.Data = value;
+        
+        if (this.Root == null)//Tree is empty
+            this.Root = newNode;
+        
+        else
+        { 
+            Node currentNode = new Node();
+            currentNode = this.Root; 
+            while (true)
+            {
+                if (value < currentNode.Data) //Is new node in left tree? 
+                {
+                    if (currentNode.LeftNode == null)
+                    {
+                        currentNode.LeftNode = newNode;
+                    }
+                    currentNode = currentNode.LeftNode; 
+                }
+                
+                if (value > currentNode.Data) //Is new node in right tree?
+                {
+                    if (currentNode.RightNode == null)
+                    {
+                        currentNode.RightNode = newNode;
+                    }
+                    currentNode = currentNode.RightNode;
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+ 
+    public Node Find(int value)
+    {
+        return this.Find(value, this.Root);            
+    }
+ 
+    public void Remove(int value)
+    {
+        this.Root = Remove(this.Root, value);
+    }
+ 
+    private Node Remove(Node parent, int key)
+    {
+        if (parent == null) return parent;
+ 
+        if (key < parent.Data) parent.LeftNode = Remove(parent.LeftNode, key); 
+        else if (key > parent.Data)
+            parent.RightNode = Remove(parent.RightNode, key);
+ 
+        // if value is same as parent's value, then this is the node to be deleted  
+        else
+        {
+            // node with only one child or no child  
+            if (parent.LeftNode == null)
+                return parent.RightNode;
+            else if (parent.RightNode == null)
+                return parent.LeftNode;
+ 
+            // node with two children: Get the inorder successor (smallest in the right subtree)  
+            parent.Data = MinValue(parent.RightNode);
+ 
+            // Delete the inorder successor  
+            parent.RightNode = Remove(parent.RightNode, parent.Data);
+        }
+ 
+        return parent;
+    }
+ 
+    private int MinValue(Node node)
+    {
+        int minv = node.Data;
+ 
+        while (node.LeftNode != null)
+        {
+            minv = node.LeftNode.Data;
+            node = node.LeftNode;
+        }
+ 
+        return minv;
+    }
+ 
+    private Node Find(int value, Node parent)
+    {
+        if (parent != null)
+        {
+            if (value == parent.Data) return parent;
+            if (value < parent.Data)
+                return Find(value, parent.LeftNode);
+            else
+                return Find(value, parent.RightNode);
+        }
+ 
+        return null;
+    }
+ 
+    public int GetTreeDepth()
+    {
+        return this.GetTreeDepth(this.Root);
+    }
+ 
+    private int GetTreeDepth(Node parent)
+    {
+        return parent == null ? 0 : Math.Max(GetTreeDepth(parent.LeftNode), GetTreeDepth(parent.RightNode)) + 1;
+    }
+ 
+    public void TraversePreOrder(Node parent)
+    {
+        if (parent != null)
+        {
+            Console.Write(parent.Data + " ");
+            TraversePreOrder(parent.LeftNode);
+            TraversePreOrder(parent.RightNode);
+        }
+    }
+ 
+    public void TraverseInOrder(Node parent)
+    {
+        if (parent != null)
+        {
+            TraverseInOrder(parent.LeftNode);
+            Console.Write(parent.Data + " ");
+            TraverseInOrder(parent.RightNode);
+        }
+    }
+ 
+    public void TraversePostOrder(Node parent)
+    {
+        if (parent != null)
+        {
+            TraversePostOrder(parent.LeftNode);
+            TraversePostOrder(parent.RightNode);
+            Console.Write(parent.Data + " ");
+        }
+    }
+}
+
+class MainClass {
+    static void Main() 
+    {
+        BinaryTree binaryTree = new BinaryTree();
+         
+        binaryTree.Insert(1);
+        binaryTree.Insert(2);
+        binaryTree.Insert(7);
+        binaryTree.Insert(3);
+        binaryTree.Insert(10);
+        binaryTree.Insert(5);
+        binaryTree.Insert(89);
+         
+        Node node = binaryTree.Find(5);
+        int depth = binaryTree.GetTreeDepth();
+         
+        Console.WriteLine("PreOrder Traversal:");
+        binaryTree.TraversePreOrder(binaryTree.Root);
+        Console.WriteLine(" \n");
+         
+        Console.WriteLine("InOrder Traversal:");
+        binaryTree.TraverseInOrder(binaryTree.Root);
+        Console.WriteLine(" \n");
+         
+        Console.WriteLine("PostOrder Traversal:");
+        binaryTree.TraversePostOrder(binaryTree.Root);
+        Console.WriteLine(" \n");
+         
+        binaryTree.Remove(7);
+        binaryTree.Remove(8);
+         
+        Console.WriteLine("PreOrder Traversal After Removing Operation:");
+        binaryTree.TraversePreOrder(binaryTree.Root);
+        Console.WriteLine();
+    }
+}
+ 
